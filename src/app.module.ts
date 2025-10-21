167a587c6b0ed43a446cb002e4bec3807f7d44f2
Dessert99 <lsjh1234@naver.com>
2025-10-22 00:42:31 +0900

    build(db): postgres DB연결
    - .env에서 환경변수 처리하였다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), //.env로드
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true, // 개발용에서만 true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
